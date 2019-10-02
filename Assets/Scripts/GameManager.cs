34cc2046c0bf8e002edb1d3c6b392ccde9848aab
sondrex <sondrewr@stud.ntnu.no>
2019-10-02 16:32:31 +0200

    Added a nearly empty game manager and made an input manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static InputManager inputManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (inputManager == null)
+        {
+            inputManager = new InputManager();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // DEBUG
+        if (Input.GetKey(inputManager.bindings[(int)InputManager.Actions.FORWARDS]))  // FORWARDS
+        {
+            Debug.Log("FORWARDS!");
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+
+    public enum Actions
+    {
+        FORWARDS,
+        BACKWARDS,
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+        ZOOM
+    };
+
+    public KeyCode[] bindings;
+
+    static InputManager instance;
+
+
+    // Initial keyInput values, run on startup
+    public InputManager()
+    {
+        bindings = new KeyCode[System.Enum.GetValues(typeof(Actions)).Length];
+
+        bindings[(int)Actions.FORWARDS] = getKeyCodeFromPlayerPrefs(Actions.FORWARDS, KeyCode.W);
+        bindings[(int)Actions.LEFT] = getKeyCodeFromPlayerPrefs(Actions.LEFT, KeyCode.A);
+        bindings[(int)Actions.BACKWARDS] = getKeyCodeFromPlayerPrefs(Actions.BACKWARDS, KeyCode.S);
+        bindings[(int)Actions.RIGHT] = getKeyCodeFromPlayerPrefs(Actions.RIGHT, KeyCode.D);
+        bindings[(int)Actions.DOWN] = getKeyCodeFromPlayerPrefs(Actions.DOWN, KeyCode.LeftShift);
+        bindings[(int)Actions.ZOOM] = getKeyCodeFromPlayerPrefs(Actions.ZOOM, KeyCode.LeftControl);
+    }
+
+    // Changes selected control to the specified key-code
+    public void changeControl(Actions action, KeyCode keyCode)
+    {
+        bindings[(int)action] = getKeyCodeFromPlayerPrefs(action, keyCode);
+    }
+
+    private KeyCode getKeyCodeFromPlayerPrefs(Actions action, KeyCode defaultValue)
+    {
+        return (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(System.Enum.GetName(typeof(Actions), action), System.Enum.GetName(typeof(KeyCode), defaultValue)));
+    }
+}
