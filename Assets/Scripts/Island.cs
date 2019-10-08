1ba74fb86dc972a23fe00c67cb297d1622e3d95f
ScarredSceptile <jonas.haegd@gmail.com>
2019-10-09 00:12:14 +0200

    First attempt at using diamond square for generating islands

--- Assets/Scripts/Island.cs
+++ Assets/Scripts/Island.cs
@@ -17,6 +17,11 @@ public class Island : MonoBehaviour
 
     public List<IslandTile> tiles;
 
+    private int size = 33;
+    private float[,] dataArray;
+    private GameObject terrain;
+    private TerrainData _terrainData;
+
     public Island(int x, int z, int islandID)
     {
         tiles = new List<IslandTile>();
@@ -41,11 +46,18 @@ public class Island : MonoBehaviour
 
         IslandTile tile = new IslandTile(x * 75 + xOffSet, z * 75 + xOffSet);
 
+        terrain = new GameObject();
+        _terrainData = new TerrainData();
+        DiamondSquare();
+        _terrainData.SetHeights(0, 0, dataArray);
+        terrain = Terrain.CreateTerrainGameObject(_terrainData);
+        terrain.transform.position = new Vector3(x * 75 + xOffSet, 2, z * 75 + xOffSet);
+
         tiles.Add(tile);
 
         tile = null;
 
-        while (tilesPlaced < 75)
+        /*while (tilesPlaced < 75)
         {
             bool placed = true;
 
@@ -113,12 +125,85 @@ public class Island : MonoBehaviour
 
             }
         }
+        */
 
-        for (int i = 0; i < tiles.Count; i++)
+        /*for (int i = 0; i < tiles.Count; i++)
         {
 
             Vector3 vec3 = new Vector3((float)tiles[i].xPos / 10f, 1, (float)tiles[i].zPos / 10f);
             Instantiate(GameObject.FindWithTag("Island"), vec3, GameObject.FindWithTag("Island").transform.rotation);
         }
+        */
+        /*
+        Vector3 vec3 = new Vector3(x * 75 + xOffSet, 1, z * 75 + xOffSet);
+        Instantiate(terrain, vec3, Quaternion.identity);
+        */
+    }
+
+    private void DiamondSquare()
+    {
+        //Declare array
+        dataArray = new float[size, size];
+
+        //Set corners
+        dataArray[0, 0] = 100;
+        dataArray[size - 1, 0] = 20;
+        dataArray[0, size - 1] = 100;
+        dataArray[size - 1, size - 1] = 50;
+
+        float h = 0.5f;
+
+        float val;
+        float rnd;
+
+        for (int i = size - 1; i >= 2; i /= 2)
+        {
+            int halfSide = i / 2;
+
+            //Square values
+            for (int x = 0; x < size - 1; x += i)
+            {
+                for (int y = 0; y < size - 1; y += i)
+                {
+                    val = dataArray[x, y];
+                    val += dataArray[x + i, y];
+                    val += dataArray[x, y + i];
+                    val += dataArray[x + i, y + i];
+
+                    val /= 4;
+
+                    rnd = (Random.value * 2.0f * h) - h;
+                    val = Mathf.Clamp01(val + rnd);
+
+                    dataArray[x + halfSide, y + halfSide] = val;
+                }
+            }
+
+
+            //Diamond values
+            for (int x = 0; x < size - 1; x += halfSide)
+            {
+                for (int y = (x + halfSide) % i; y < size - 1; y += i)
+                {
+                    val = dataArray[(x - halfSide + size - 1) % (size - 1), y];
+                    val += dataArray[(x + halfSide) % (size - 1), y];
+                    val += dataArray[x, (y + halfSide) % (size - 1)];
+                    val += dataArray[x, (y - halfSide + size - 1) % (size - 1)];
+
+                    val /= 4;
+
+                    rnd = (Random.value * 2 * h) - h;
+                    val = Mathf.Clamp01(val + rnd);
+
+                    dataArray[x, y] = val;
+
+                    if (x == 0) dataArray[size - 1, y] = val;
+                    if (y == 0) dataArray[x, size - 1] = val;
+                }
+            }
+
+            h /= 2;
+        }
+
     }
 }
