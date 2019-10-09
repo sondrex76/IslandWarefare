6a25e23dd1daa9be89683c37f57dd4243c52903a
Onhil <onhilx@gmail.com>
2019-10-09 18:07:16 +0200

    selectables, select rectangle.  need to add events

---
+++ Assets/Scripts/PlaceFlag.cs
@@ -0,0 +1,44 @@
+﻿// MoveToClickPoint.cs
+    using UnityEngine;
+    using UnityEngine.AI;
+    
+    public class PlaceFlag : MonoBehaviour {
+        NavMeshAgent agent;
+        [SerializeField]
+        float ok;
+        Selectables _isSelected;
+
+        void Start() {
+            agent = GetComponent<NavMeshAgent>();
+            Debug.Log(agent.gameObject.name);
+            _isSelected = GetComponent<Selectables>();
+        }
+        
+        void Update() {
+            if(_isSelected.isSelected) 
+            {
+                if (Input.GetMouseButtonDown(1))
+                {
+                    RaycastHit hit;
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        //suppose i have two objects here named obj1 and obj2.. how do i select obj1 to be transformed 
+                        if (hit.transform != null)
+                        {
+                        
+                            //transform.Translate(Time.deltaTime, 0, 0, Space.Self);
+                            agent.SetDestination(hit.point);
+
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /*
+    Mouse click to [SEND] target location for [SELECTED] units
+    Place [FLAG] on selected destination [FLAG's] only visible if having selected a unit going there (AOM style)
+    Move [SELECTED] [UNITS] to targeted locations with go here in order with say shit + click or just go here with click
+     */
---
+++ Assets/Scripts/Selectables.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright (c) 2016, Ivo van der Marel
+ * Released under MIT License (= free to be used for anything)
+ * Enjoy :)
+ */
+ 
+using UnityEngine;
+ 
+public class Selectables : MonoBehaviour
+{
+ 
+    internal bool isSelected
+    {
+        get
+        {
+            return _isSelected;
+        }
+        set
+        {
+            _isSelected = value;
+            //Replace this with your custom code. What do you want to happen to a Selectable when it get's (de)selected?
+            Renderer r = GetComponentInChildren<Renderer>();
+            if (r != null)
+            {
+                r.material.color = value ? Color.red : Color.white;                
+            }
+        }
+    }
+ 
+    private bool _isSelected;
+ 
+    void OnEnable()
+    {
+        RTSSelection.selectables.Add(this);
+    }
+ 
+    void OnDisable()
+    {
+        RTSSelection.selectables.Remove(this);
+    }
+ 
+}
