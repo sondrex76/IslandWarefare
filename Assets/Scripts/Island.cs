d9cc16382de70e2b85840e9905d9e3665c0058c2
ScarredSceptile <jonas.haegd@gmail.com>
2019-10-09 09:02:07 +0200

    Removed the 2d way of getting islands

--- Assets/Scripts/Island.cs
+++ Assets/Scripts/Island.cs
@@ -15,16 +15,13 @@ public class Island : MonoBehaviour
     public int southIsland;
     public int westIsland;
 
-    public List<IslandTile> tiles;
-
-    private int size = 33;
+    protected int size = 33;
     private float[,] dataArray;
     private GameObject terrain;
     private TerrainData _terrainData;
 
     public Island(int x, int z, int islandID)
     {
-        tiles = new List<IslandTile>();
 
         ID = islandID;
         xCord = x;
@@ -39,12 +36,8 @@ public class Island : MonoBehaviour
 
         //algorithm for placing the tiles
 
-        int tilesPlaced = 1;
-
         int xOffSet = Random.Range(-20, 20);
         int zOffSet = Random.Range(-20, 20);
-
-        IslandTile tile = new IslandTile(x * 75 + xOffSet, z * 75 + xOffSet);
 
         terrain = new GameObject();
         _terrainData = new TerrainData();
@@ -56,92 +49,7 @@ public class Island : MonoBehaviour
         _terrainData.SetHeights(0, 0, dataArray);
         terrain = Terrain.CreateTerrainGameObject(_terrainData);
         terrain.transform.position = new Vector3(x * 75 + xOffSet, 0, z * 75 + xOffSet);
-
-        tiles.Add(tile);
-
-        tile = null;
-
-        /*while (tilesPlaced < 75)
-        {
-            bool placed = true;
-
-            int xPos, zPos;
-
-            //get a random tile already for the island
-            int nextTo = Random.Range(0, tiles.Count);
-            int side = Random.Range(0, 4);
-
-            //Place the tile next to the chosen tile that already exist to ensure that the island is connected
-            switch (side)
-            {
-                //North
-                case 0:
-                    xPos = tiles[nextTo].xPos;
-                    zPos = tiles[nextTo].zPos + 1;
-                    break;
-
-                //West
-                case 1:
-                    xPos = tiles[nextTo].xPos - 1;
-                    zPos = tiles[nextTo].zPos;
-                    break;
-
-                //South
-                case 2:
-                    xPos = tiles[nextTo].xPos;
-                    zPos = tiles[nextTo].zPos - 1;
-                    break;
-
-                //East
-                case 3:
-                    xPos = tiles[nextTo].xPos + 1;
-                    zPos = tiles[nextTo].zPos;
-                    break;
-
-                default:
-
-                    xPos = tiles[nextTo].xPos;
-                    zPos = tiles[nextTo].zPos;
-                    break;
-            }
-
-            //Check if position is good
-
-            for (int i = 0; i < tiles.Count; i++)
-            {
-
-                //Make sure it does not overlap with any other tiles
-                if (xPos == tiles[i].xPos && zPos == tiles[i].zPos)
-                {
-                    //if the tile overlaps, it won't be placed and there is no need to check the rest of the tiles
-                    placed = false;
-                    break;
-                }
-            }
-
-            //If it can be placed, add it to the list
-            if (placed)
-            {
-                tilesPlaced++;
-                tile = new IslandTile(xPos, zPos);
-                tiles.Add(tile);
-                tile = null;
-
-            }
-        }
-        */
-
-        /*for (int i = 0; i < tiles.Count; i++)
-        {
-
-            Vector3 vec3 = new Vector3((float)tiles[i].xPos / 10f, 1, (float)tiles[i].zPos / 10f);
-            Instantiate(GameObject.FindWithTag("Island"), vec3, GameObject.FindWithTag("Island").transform.rotation);
-        }
-        */
-        /*
-        Vector3 vec3 = new Vector3(x * 75 + xOffSet, 1, z * 75 + xOffSet);
-        Instantiate(terrain, vec3, Quaternion.identity);
-        */
+        
     }
 
     private void DiamondSquare()
