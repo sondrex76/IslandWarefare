78da8a5883dfe09f58c5617f4d4856c2a5b29378
sondrex <sondrewr@stud.ntnu.no>
2019-10-25 15:30:46 +0200

    Made various functions inside BuildingTemplate 'protected void' and made an empty ResourceHarvestingTemplate

--- Assets/Scripts/Buildings/BuildingTemplate.cs
+++ Assets/Scripts/Buildings/BuildingTemplate.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 
-/* Template for buildings
- * To make a building a model for the building must be added to the game object
-*/
+// Template for buildings
+// To make a building a model for the building must be added to the game object
+// 
+
 public class BuildingTemplate : MonoBehaviour
 {
     GameManager gameManager;                // Game manager object, is used in child objects to modify resource amounts
@@ -11,20 +12,21 @@ public class BuildingTemplate : MonoBehaviour
     [SerializeField] float maxHealth;       // Max health for building
     [SerializeField] float currentHealth;   // Current health
 
-    private void Start()
+    protected void Awake()
     {
         gameManager = FindObjectOfType<GameManager>();
     }
 
     // Updates 50 times a second independent of framerate
-    private void FixedUpdate()
+    protected void FixedUpdate()
     {
         // Runs the building's primary functionality
         BuildingFunctionality();
     }
 
+
     // How much health should the building take, negative value for healing
-    public bool HurtBuilding(float lostHealth)  // Returns true if building is destroyed
+    protected virtual bool HurtBuilding(float lostHealth)  // Returns true if building is destroyed
     {
         currentHealth -= lostHealth;
 
@@ -39,19 +41,19 @@ public class BuildingTemplate : MonoBehaviour
     }
 
     // The primary functionality run every fixed update
-    void BuildingFunctionality()
+    protected virtual void BuildingFunctionality()
     {
-        // Fill in in child, this code wil lvary from building to building
+        // Fill in in child, this code will vary from building to building
     }
 
     // What do do when building reaches 0 hp, overwrite in child class for more complex behaviour
-    void ZeroHealth()
+    protected virtual void ZeroHealth()
     {
         DestroyBuilding();  // Destroys building
     }
 
     // Destroys this building, you can send it the coordinates 
-    void DestroyBuilding(float x = 0, float y = 0, float z = 0, float rotation = 0)
+    protected virtual void DestroyBuilding(float x = 0, float y = 0, float z = 0, float rotation = 0)
     {
         // By default the building will place a prefab at the relative coordinates of (0, 0, 0), will not happen if prefab is not defined
         if (prefab != null)
@@ -63,7 +65,7 @@ public class BuildingTemplate : MonoBehaviour
 
     // Loads a prefab, at relative x, y, z coordinates rotated around the y axis by rotation comapred to building
     // Is used with a check to see if the prefab is null first: if (prefab != null) {}
-    void LoadPrefab(GameObject gameObject, Vector3 coordinates, float rotation)
+    protected virtual void LoadPrefab(GameObject gameObject, Vector3 coordinates, float rotation)
     {
         GameObject newObject = Instantiate(gameObject, coordinates, Quaternion.identity);   // Places object
         newObject.transform.Rotate(0, rotation, 0);                                         // Rotates object
---
+++ Assets/Scripts/Buildings/ResourceHarvestingTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Resource gathering building template
+public class ResourceHarvestingTemplate : BuildingTemplate
+{
+    // Code that runs once when the resource gathering building is placed
+    private void Start()
+    {
+        
+    }
+    protected override void BuildingFunctionality()
+    {
+
+    }
+}
