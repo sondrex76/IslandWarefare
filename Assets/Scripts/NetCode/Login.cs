8dd18dcaa9560fca54edbcfe08e1d2ffe0285b47
bjornaah <bjornaah@stud.ntnu.no>
2019-10-27 02:54:08 +0100

    Assign island to player in playfab

--- Assets/Scripts/NetCode/Login.cs
+++ Assets/Scripts/NetCode/Login.cs
@@ -5,6 +5,12 @@ using PlayFab;
 using PlayFab.ClientModels;
 using PlayFab.DataModels;
 using PlayFab.ProfilesModels;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 public class Login : MonoBehaviour
 {
@@ -12,11 +18,14 @@ public class Login : MonoBehaviour
     private string email;
     private string password;
     private string username;
+    private string url = "https://F5079.playfabapi.com/Server/GetPlayersInSegment";
     public GameObject loginPanel;
 
 
     public void Start()
     {
+
+
 
         //Setting the title ID so playfab knows what endpoint to connect to just incase something buggs in editor
         if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
@@ -24,7 +33,7 @@ public class Login : MonoBehaviour
             PlayFabSettings.TitleId = "F5079";
         }
 
-
+        //Check if user got credentials saved
         if (PlayerPrefs.HasKey("EMAIL"))
         {
 
@@ -39,7 +48,7 @@ public class Login : MonoBehaviour
     }
 
 
-
+    //Login is successfull and saves email and password
     void OnLoginSuccess(LoginResult result)
     {
         Debug.Log("Login successfull");
@@ -47,6 +56,7 @@ public class Login : MonoBehaviour
         PlayerPrefs.SetString("PASSWORD", password);
         loginPanel.SetActive(false);
     }
+
 
     void OnLoginFail(PlayFabError error)
     {
@@ -56,11 +66,13 @@ public class Login : MonoBehaviour
         Debug.Log(error.GenerateErrorReport());
     }
 
+    //Registration is successfull and saves email and password
     void OnRegisterSuccess(RegisterPlayFabUserResult result)
     {
         PlayerPrefs.SetString("EMAIL", email);
         //Is this a safe way to store passwords?
         PlayerPrefs.SetString("PASSWORD", password);
+        GetPlayers();
         loginPanel.SetActive(false);
     }
 
@@ -71,23 +83,26 @@ public class Login : MonoBehaviour
 
 
 
+    //Sets email to the email typed in the email box
     public void GetEmail(string emailText)
     {
         email = emailText;
     }
 
+    //Sets username to the username typed in the username box
     public void GetUsername(string usernameText)
     {
         username = usernameText;
     }
 
+    //Sets password to the password typed in the password box
     public void GetPassword(string passwordText)
     {
         password = passwordText;
     }
 
 
-
+    //Starts the login and registration process
     public void LoginClicked()
     {
 
@@ -108,4 +123,86 @@ public class Login : MonoBehaviour
     {
         loginPanel.SetActive(false);
     }
+
+    public void GetPlayers()
+    {
+        GetAllPlayerSegments();
+    }
+
+    public async Task GetAllPlayerSegments()
+    {
+        var httpClient = new HttpClient();
+        httpClient.BaseAddress = new System.Uri(url);
+
+        //Setting the headers
+        httpClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        httpClient.DefaultRequestHeaders.Add(
+            "X-SecretKey", "BFAUNKU49AITMY3KQCNNZ7YONC7BCS87NFOPP6Q9CQEYWCTGUX");
+
+        //Setting content to be posted
+        var values = new Dictionary<string, string>
+        {
+        {"SegmentId", "2C3F0B4487C23F69"},
+        {"SecondsToLive", "5"},
+        {"MaxBatchSize", "500"}
+        };
+
+        var json = JsonConvert.SerializeObject(values);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //Sends POST request to endpoint
+        HttpResponseMessage httpResponse = await httpClient.PostAsync(url, content);
+        //Get the response as a string
+        var responseString = await httpResponse.Content.ReadAsStringAsync();
+        Debug.Log(responseString);
+
+        //Parse the string into Objects
+        dynamic response = JsonConvert.DeserializeObject<RootObject>(responseString);
+
+
+        setPlayerData(response.data.ProfilesInSegment);
+
+
+    }
+
+    //Set the player data
+    void setPlayerData(int numberOfPlayers)
+    {
+
+        //Set the playerdata in playfab
+        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
+        {
+            Data = new Dictionary<string, string>()
+            {
+            {"IslandID", numberOfPlayers.ToString()}
+            }
+        },
+            result => Debug.Log("Yay you got an island"),
+            error =>
+            {
+                Debug.Log("Error setting playerdata");
+                Debug.Log(error.GenerateErrorReport());
+            }
+            );
+
+    }
+
+
+    //Response structure for playersinsegment
+    public class Data
+    {
+        public int ProfilesInSegment { get; set; }
+        public List<object> PlayerProfiles { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int code { get; set; }
+        public string status { get; set; }
+        public Data data { get; set; }
+    }
+
+
 }
