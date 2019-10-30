3fcf4423c34e907be2ca785512bc038535f465ff
Onhil <onhilx@gmail.com>
2019-10-30 15:33:32 +0100

    can now make a list of destination using left shift as a modifier. agent will move to the next destination after reaching a new one. (currently not efficient as its checking per frame) can cancel list with a normal destination
    note to self dont commit, pull then undo commit

--- Assets/Scripts/Object placement/PlaceFlag.cs
+++ Assets/Scripts/Object placement/PlaceFlag.cs
@@ -4,28 +4,33 @@
     using System.Collections.Generic;
     
     public class PlaceFlag : MonoBehaviour {
-        [SerializeField]
-        float ok;
+        bool _list;
         EventManager _eventManager;
         void Start() {
+            _list = false;
             _eventManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<EventManager>();
         }
         
         void Update() {
             if (Input.GetButtonDown("Fire2"))
             {
+                // GetButton locking something? ketkey for now
+                _list = Input.GetKey(KeyCode.LeftShift);
                 RaycastHit hit;
                 if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
                 {
+                    
                     //suppose i have two objects here named obj1 and obj2.. how do i select obj1 to be transformed 
                     if (hit.transform != null)
                     {
-                        
+                      
                         //transform.Translate(Time.deltaTime, 0, 0, Space.Self);
-                        _eventManager._listenToFlag.Invoke(hit.point, true); 
+                        _eventManager._listenToFlag.Invoke(hit.point, _list); 
                            
                     }
-                }                
+                } 
+                
+                        
             }
         }
     }
