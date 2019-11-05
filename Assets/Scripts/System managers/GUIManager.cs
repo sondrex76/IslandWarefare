a93f7596346cfbc51ca6e008a11df383c38fe0d3
sondrex <sondrewr@stud.ntnu.no>
2019-11-05 12:56:36 +0100

    Added sprites for happiness level and population and made sprite for happiness level depend on actual level of happiness(between five different states)

--- Assets/Scripts/System managers/GUIManager.cs
+++ Assets/Scripts/System managers/GUIManager.cs
@@ -7,7 +7,12 @@ public class GUIManager : MonoBehaviour
     [SerializeField] Text populationLabel;
     [SerializeField] Text happyLabel;
     [SerializeField] GameManager gameManager;
+    [SerializeField] Image happinessLevelsImage;
+
+
+    [SerializeField] Sprite[] sprites;
     
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -16,8 +21,28 @@ public class GUIManager : MonoBehaviour
         //happyLabel.text = gameManager.happiness + "";
         
         moneyLabel.text = "$" + System.String.Format("{0:n}", gameManager.moneyAmount);
-        populationLabel.text = "P: " + System.String.Format("{0:n}", gameManager.population);
-        happyLabel.text = "H: " + System.String.Format("{0:n}", gameManager.happiness);
+        populationLabel.text = System.String.Format("{0:n}", gameManager.population);
+        happyLabel.text = System.String.Format("{0:n}", gameManager.happiness);
+
+        if (gameManager.happiness >= 90)        // Happy
+        {
+            happinessLevelsImage.sprite = sprites[0];
+        } else if (gameManager.happiness >= 66) // Content
+        {
+            happinessLevelsImage.sprite = sprites[1];
+        }
+        else if (gameManager.happiness >= 33)   // Middeling
+        {
+            happinessLevelsImage.sprite = sprites[2];
+        }
+        else if (gameManager.happiness >= 15)   // Somewhat displeased
+        {
+            happinessLevelsImage.sprite = sprites[3];
+        } else                                  // Unhappy
+        {
+            happinessLevelsImage.sprite = sprites[4];
+        }
+
         
         // happyLabel.text = gameManager.happiness + "%";
     }
