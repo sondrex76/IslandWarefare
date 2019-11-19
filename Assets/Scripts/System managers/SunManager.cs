4703fd6693c59a9a11643e433432f08ded3fafd1
sondrex <sondrewr@stud.ntnu.no>
2019-11-19 10:32:38 +0100

    Added sun object that rotates, currently no graphical representation, just lghting

---
+++ Assets/Scripts/System managers/SunManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunManager : MonoBehaviour
+{
+    [SerializeField] Light sunObject;           // Sun
+    [SerializeField] float timeRotationSeconds; // Time to rotate object
+
+    float timeValue;
+    private void Start()
+    {
+        timeValue = 360 / 50 / timeRotationSeconds;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // If game manager is not paused the sun will be moved
+        if (!GameManager.isPaused)
+        {
+            print(sunObject.transform.eulerAngles.x);
+
+            sunObject.transform.Rotate(timeValue, 0, 0);
+        }   
+    }
+}
