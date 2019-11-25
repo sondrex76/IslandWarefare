9f609dd9fcfaa002dd69117f3a343c3b2f4e6964
bjornaah <bjornaah@stud.ntnu.no>
2019-11-25 09:18:01 +0100

    Simple camera island map

---
+++ Assets/Scripts/CameraControllMapView.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllMapView : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+    InputManager inputManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = GameManager.inputManager;
+        Debug.Log(inputManager.bindings);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(inputManager.bindings[(int)InputManager.Actions.BACKWARDS]))
+        {
+            transform.Translate(transform.forward * speed);
+        }
+
+
+        if (Input.GetKey(inputManager.bindings[(int)InputManager.Actions.FORWARDS]))
+        {
+            transform.Translate(transform.forward * -speed);
+        }
+
+
+        if (Input.GetKey(inputManager.bindings[(int)InputManager.Actions.LEFT]))
+        {
+            transform.Translate(transform.right * -speed);
+        }
+
+        if (Input.GetKey(inputManager.bindings[(int)InputManager.Actions.RIGHT]))
+        {
+            transform.Translate(transform.right * speed);
+        }
+
+    }
+}
