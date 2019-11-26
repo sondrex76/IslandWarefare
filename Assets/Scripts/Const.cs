2fe489ce1ac74280b38c2a746ab34af11620a674
ScarredSceptile <jonas.haegd@gmail.com>
2019-11-26 21:06:32 +0100

    Added a few comments

--- Assets/Scripts/Const.cs
+++ Assets/Scripts/Const.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class Const
 {
-    public const int size = 255;        //Size of islands
-    public const int islandHeight = 200;
-    public const int islandGradient = 35;
-    public const int islandDistance = 4000;
+    public const int size = 255;                //Size of islands
+    public const int islandHeight = 200;        //Height of the islands
+    public const int islandGradient = 35;       //How soft the islands are. Harder means more up and down
+    public const int islandDistance = 4000;     //Distance between the islands
 }
