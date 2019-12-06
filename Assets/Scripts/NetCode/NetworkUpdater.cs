84d1e44746876e1badaad0d4538cfe4d280a4429
bjornaah <bjornaah@stud.ntnu.no>
2019-12-06 14:32:01 +0100

    Now display the rigt amount of islands on island view again

---
+++ Assets/Scripts/NetCode/NetworkUpdater.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class NetworkUpdater : MonoBehaviour
+{
+
+    NetworkHelpers networkhelper = new NetworkHelpers();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    private void Start()
+    {
+        GetPlayersLogin();
+    }
+
+
+    public async Task GetPlayersLogin()
+    {
+        await networkhelper.GetAllPlayerSegments(setNumberOfPlayers);
+    }
+
+
+
+    void setNumberOfPlayers(int numberOfPlayers)
+    {
+        PlayerPrefs.SetInt("NumberOfPlayers", 100);
+    }
+
+
+}
