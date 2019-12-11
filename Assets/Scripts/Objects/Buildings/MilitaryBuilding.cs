dacf850b06c9249707487002ed4fcf0f98afc18b
bjornaah <bjornaah@stud.ntnu.no>
2019-12-12 00:20:00 +0100

    Added military building, and units you can buy

---
+++ Assets/Scripts/Objects/Buildings/MilitaryBuilding.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class MilitaryBuilding : FactoryBuilding
+{
+
+    [SerializeField] GameObject gui;                    // GUI element
+
+    [SerializeField]
+    MilitaryUnit[] units;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gui == null)                    // Checks if GUI have already been set
+            gui = GameObject.Find("GUI");   // Sets GUI
+
+        ActivateGUI(false);
+    }
+
+
+    protected override void MakeNode()
+    {
+        // Move code below
+        ObjectPool pool = GameObject.FindGameObjectWithTag("Manager").GetComponent<ObjectPool>();
+        GameObject graph = GameObject.FindGameObjectWithTag("Graph");
+        node = pool.GetPooledObject("OfficeNode");
+
+        node.transform.position = new Vector3(transform.position.x, Mathf.Floor(transform.position.y), transform.position.z);
+        node.transform.parent = graph.transform;
+        node.SetActive(true);
+        graph.GetComponent<Graph>().AddNodes();
+    }
+}
