8f74bfdd18666d5158fae4d2c69fc0225374f883
bjornaah <bjornaah@stud.ntnu.no>
2019-12-12 16:34:40 +0100

    Moved scripts

--- Assets/Scripts/SetButtonOnClick.cs
+++ Assets/Scripts/SetButtonOnClick.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class setButtonOnClick : MonoBehaviour
+public class SetButtonOnClick : MonoBehaviour
 {
     [SerializeField]
     GameManager gameManager;
