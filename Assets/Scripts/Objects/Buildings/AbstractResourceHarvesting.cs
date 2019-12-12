4949f5cc921c44ed52117b7390d6dd485d031742
sondrex <sondrewr@stud.ntnu.no>
2019-12-12 20:19:12 +0100

    Fixed resource gatherers breaking things

--- Assets/Scripts/Objects/Buildings/AbstractResourceHarvesting.cs
+++ Assets/Scripts/Objects/Buildings/AbstractResourceHarvesting.cs
@@ -55,6 +55,21 @@ public class AbstractResourceHarvesting : AbstractBuilding
         else return 0;
     }
 
+
+    // Overwrites MakeNode()
+    protected override void MakeNode()
+    {
+        // Move code below
+        ObjectPool pool = GameObject.FindGameObjectWithTag("Manager").GetComponent<ObjectPool>();
+        GameObject graph = GameObject.FindGameObjectWithTag("Graph");
+        node = pool.GetPooledObject("OfficeNode");
+
+        node.transform.position = new Vector3(transform.position.x, Mathf.Floor(transform.position.y), transform.position.z);
+        node.transform.parent = graph.transform;
+        node.SetActive(true);
+        graph.GetComponent<Graph>().AddNodes();
+    }
+
     // checks if resource is there
     private void OnTriggerStay(Collider other)
     {
